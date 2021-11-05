3f23ee6058305c88f5477ba2ef11d9ba6c85683d
Kenneth Ros <zhaho.ros@gmail.com>
2021-11-05 13:24:49 +0100

    Initial

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+    // Movement speed
+    public float speed = 2;
+
+    // Flap force
+    public float force = 300;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // Restart
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    // Use this for initialization
+    void Start () {
+        // Fly towards the right
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Flap
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+    // Movement Speed (0 means don't move)
+    public float speed = 0;
+
+    // Switch Movement Direction every x seconds
+    public float switchTime = 2;
+
+    // Use this for initialization
+    void Start () {
+        // Initial Movement Direction
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+
+        // Switch every few seconds
+        InvokeRepeating("Switch", 0, switchTime);
+    }
+
+    void Switch() {
+        GetComponent<Rigidbody2D>().velocity *= -1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
